5de2e368813fe3e7f385f3b7a9f3c8ae55dcffb2
Ben Greenberg <bengreenberg@gmail.com>
2022-05-31 17:51:41 +0200

    first work on adding Comment class and adding new comment to action

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import * as core from "@actions/core";
 
 import getInput from "./lib/input";
 import { Discussion } from "./lib/discussion";
+import { Comment } from "./lib/comment";
 
 export default async function run(): Promise<void> {
   try {
@@ -13,9 +14,14 @@ export default async function run(): Promise<void> {
     const discussion = new Discussion(repositoryId, categoryId, title, body);
     await discussion.save();
 
+    const commentBody = getInput("comment");
+    const newComment = new Comment(commentBody, discussion.id);
+    await newComment.save();
+
     // Set commit sha output
     core.setOutput("discussion-id", discussion.id);
     core.setOutput("discussion-url", discussion.url);
+    core.setOutput("comment-id", newComment.id);
   } catch (e) {
     core.debug(e.stack);
     core.setFailed(e);
